25c70dcb0bf7bad46e82b3598108408698314b0e
TaherAmlaki <taher.amlaki@gmail.com>
2021-04-02 12:07:16 +0200

    Fixed bug on root-dir arg for to start WireMock

--- WireMockTutorial/Utils/WireMockContextManager.py
+++ WireMockTutorial/Utils/WireMockContextManager.py
@@ -15,9 +15,10 @@ class WireMockContext:
     def __init__(self, jar_location: str, root_dir: str, port: int = 9999, verbose: bool = True):
         self.jar_location = Path(jar_location).absolute()
         self.root_dir = Path(root_dir).absolute()
-        self.command = f"java -jar {jar_location} --port {port} --global-response-templating --root-dir {root_dir}"
+        self.command = f"java -jar {jar_location} --port {port} " \
+                       f"--global-response-templating --root-dir {root_dir}"
         if verbose:
-            self.command += "--verbose"
+            self.command += " --verbose"
 
     def __enter__(self):
         p = Popen(self.command.split(), stdout=PIPE, stderr=STDOUT)
@@ -31,7 +32,6 @@ class WireMockContext:
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type:
             logger.exception(f"Error while running WireMock: {exc_type.__name__}: {exc_val}")
-        self.reset_mappings_and_requests_log()
         self.shutdown_wire_mock()
         return False
 
